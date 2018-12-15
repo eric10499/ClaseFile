fe852c1bd3e6ac13bb6bbd46098ffd99be24083a
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-15 18:12:18 +0100

    Update VerDir6.java

--- VerDir6.java
+++ VerDir6.java
@@ -1,3 +1,29 @@
+/*
+ * VerDir6.java
+ * 
+ * Copyright 2018 ERIC <ERIC@DESKTOP-5T1N085>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * ● Copia el ejemplo anterior y ejecútalo ● Realiza un programa que elimine el directorio creado en el punto anterior. Para ello habrás de eliminar todos los archivos que se encuentren dentro del directorio.
+ * 
+ * Fet per Eric Visier Sánchez
+ * 
+ */
+
 package m9;
 
 import java.io.*;
